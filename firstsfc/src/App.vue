b1b04e04935ddad384af6f1f3e7a940e92f70005
Chase Ian Famisaran <clfamisaran@student.apc.edu.ph>
2026-01-16 08:32:34 +0800

    changed and updated styles

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -6,12 +6,3 @@
 
 <script></script>
 
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-</style>
