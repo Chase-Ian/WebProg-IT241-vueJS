ffb2ee367e481b4b1dea2df8dc3ebf8409c52ba1
Chase Ian Famisaran <clfamisaran@student.apc.edu.ph>
2026-01-16 08:23:38 +0800

    added fooditem2

---
+++ firstsfc/src/components/FoodItem2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Burger',
+      message: 'I like burger'
+    }
+  }
+};
+</script>
+
+<style></style>
