a2c213264d16c51e10e53b217e30b4ac8bba9131
Chase Ian Famisaran <clfamisaran@student.apc.edu.ph>
2026-01-27 09:39:06 +0800

    revert back to previous align

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,24 +1,19 @@
 <template>
-  <div class="app-wrapper">
-    <div class="app-stack">
-      <personal-profile />
-      <commentform />
-      <comment />
-    </div>
+  <div class="app-stack">
+    <personal-profile />
+    <commentform />
+    <comment />
   </div>
 </template>
 
-<style>
-.app-wrapper {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center; /* vertical */
-  align-items: center;     /* horizontal */
-}
+<script>
+export default {}
+</script>
 
+<style>
 .app-stack {
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  gap: 1rem; /* optional spacing */
 }
 </style>
