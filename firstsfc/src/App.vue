6d1d1568af29b348184eb3a8925d651153ae1e4a
enderplayz05 <chaseian05@gmail.com>
2026-01-27 10:09:47 +0800

    updated path

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script>
-import './bootstrap/dist/css/bootstrap.min.css'
-import './bootstrap/dist/js/bootstrap.bundle.min.js'
+import '/bootstrap/dist/css/bootstrap.min.css'
+import '/bootstrap/dist/js/bootstrap.bundle.min.js'
 export default {}
 </script>
 
