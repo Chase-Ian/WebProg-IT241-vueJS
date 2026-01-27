1a2fd382f015d43a2b586e4f049594a2b4854282
Chase Ian Famisaran <clfamisaran@student.apc.edu.ph>
2026-01-27 09:09:20 +0800

    Task 03 Add view and insert comment/visitor feedback functionality to your website

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -4,9 +4,14 @@ import App from './App.vue'
 import personal_profile from './components/personal_profile.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
+import Comment from './components/Comment.vue'
+import CommentForm from './components/CommentForm.vue'
+
 
 const app = createApp(App)
 app.component('personal-profile',personal_profile)
 app.component('food-item', FoodItem)
 app.component('food-item2',FoodItem2)
+app.component('comment',Comment)
+app.component('commentForm',CommentForm)
 app.mount('#app')
