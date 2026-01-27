50eafc77fe5624e9a4b3b2ea9414237af607de62
Chase Ian Famisaran <clfamisaran@student.apc.edu.ph>
2026-01-27 09:36:46 +0800

    again app stack middle fix

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="app-stack">
-    <personal-profile />
-    <commentform />
-    <comment />
+  <div class="app-wrapper">
+    <div class="app-stack">
+      <personal-profile />
+      <commentform />
+      <comment />
+    </div>
   </div>
 </template>
 
-<script>
-export default {}
-</script>
-
 <style>
+.app-wrapper {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center; /* vertical */
+  align-items: center;     /* horizontal */
+}
+
 .app-stack {
   display: flex;
   flex-direction: column;
-  gap: 1rem; 
-  align-items: center;
+  gap: 1rem;
 }
 </style>
