e5c6f79c74f70c225a2f746bc859aa0149d9e8ee
Chase Ian Famisaran <clfamisaran@student.apc.edu.ph>
2026-01-27 09:32:32 +0800

    app stacking

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <personal-profile/>
-  <commentform/>
-  <comment/>
-   
-
+  <div class="app-stack">
+    <personal-profile />
+    <commentform />
+    <comment />
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {}
+</script>
+
+<style>
+.app-stack {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem; /* optional spacing */
+}
+</style>
